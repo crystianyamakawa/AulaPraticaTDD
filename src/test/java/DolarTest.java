495af1ab0375cf5725891d25932c4520f1febee5
Crystian Yamakawa <crystian_yamakawa@hotmail.com>
2019-11-20 22:14:50 -0300

    parte 6

--- src/test/java/DolarTest.java
+++ src/test/java/DolarTest.java
@@ -12,16 +12,18 @@ public class DolarTest {
 		   assertEquals(Money.dollar(10), five.times(2));
 		   assertEquals(Money.dollar(15), five.times(3));
 		}
-	
-	@Test
-	public void testEquality() {
-//		   assertTrue(new Dollar(5).equals(new Dollar(5)));
-//		   assertFalse(new Dollar(5).equals(new Dollar(6)));
-		}
-	
 	@Test
 	public void testCurrency() {
 		   assertEquals("USD", Money.dollar(1).currency());
 		   assertEquals("CHF", Money.franc(1).currency());
 		}
+	
+	@Test
+	public void testEquality() {
+		   assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
+		   assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+//		   assertFalse(Money.franc(5).equals(Money.dollar(5)));
+		}	
+	
+	
 }	
--- src/test/java/FrancTest.java
+++ src/test/java/FrancTest.java
@@ -5,19 +5,11 @@ import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class FrancTest {
-	@Test
-	public void testFrancMultiplication() {
-	//	   Franc five = new Franc(5);
-	//	   assertEquals(new Franc(10), five.times(2));
-	//	   assertEquals(new Franc(15), five.times(3));
-		}
 
 	@Test
 	public void testEquality() {
-		   assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		   assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
 		   assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		   assertTrue(Money.franc(5).equals(Money.franc(5)));
-		   assertFalse(Money.franc(5).equals(Money.franc(6)));
 		   assertFalse(Money.franc(5).equals(Money.dollar(5)));
 		}
 	
